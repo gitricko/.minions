ccab79a3462c530d21e05d5bf9a9c4328b5be9e4
oclawricko <jpmcricko@gmail.com>
2026-09-03 22:35:26 +0000

    feat: log pi-failover install in boot.sh

--- boot.sh
+++ boot.sh
@@ -178,7 +178,8 @@ if [ "${DRY_RUN}" -eq 0 ]; then
     pi_update_config "${OMNIROUTE_PORT}" "${MODELRELAY_PORT}"
     
     # Install pi-failover extension (needs config in place and proxies running)
-    if "${MINIONS_HOME}/bin/pi" install git:github.com/gitricko/pi-failover@hermes-impl 2>/dev/null; then
+    log_info "Installing pi-failover extension..."
+    if "${MINIONS_HOME}/bin/pi" install git:github.com/gitricko/pi-failover@hermes-impl >> "${boot_log}" 2>&1; then
         log_info "pi-failover extension installed"
     else
         log_warn "pi-failover extension install failed (non-fatal)"
