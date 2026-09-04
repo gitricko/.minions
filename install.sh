265cc06800aace0e6bd6a67edc4797fc9836a1e9
oclawricko <jpmcricko@gmail.com>
2026-09-04 21:19:33 +0000

    fix: export MINIONS_HERMES_PRECONFIG for shellcheck

--- install.sh
+++ install.sh
@@ -211,7 +211,7 @@ ensure_modelrelay
 if [ "${INSTALL_HERMES}" -eq 1 ]; then
     log_info "Installing Hermes (opt-in)..."
     # Preconfigure Hermes (set auto-fastest model, disable login)
-    MINIONS_HERMES_PRECONFIG=1
+    export MINIONS_HERMES_PRECONFIG=1
     ensure_hermes
     
     # Update Hermes config with current ports
